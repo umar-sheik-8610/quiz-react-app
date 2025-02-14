3d57fbc5b90c08fe92c7bbed493800f36144d584
Umar <umarsheik8610@gmail.com>
2025-02-14 10:20:51 +0530

    created a template and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="quiz-app">
+        <div className="score-section" style={{ display: "none" }}>
+          <h3>Your Score : 3/3</h3>
+          <button>Restart</button>
+        </div>
+        <div className="question-section">
+          <h2>Question 1</h2>
+          <p>This is sample Questions ?</p>
+        </div>
+        <div className="options">
+          <button>Option 1</button>
+          <button>Option 2</button>
+          <button>Option 3</button>
+          <button>Option 4</button>
+        </div>
+        <div className="timer">
+          Time Left : <span>5s</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
